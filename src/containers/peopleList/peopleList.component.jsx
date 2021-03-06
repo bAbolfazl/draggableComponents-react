ecf39cf179ffbbf6bc192f45c8bd9f34e435e377
BAbolfazl <a.baghshahi1327@gmail.com>
2021-03-06 12:04:10 +0330

    remove unused pakages(react-draggable)

--- src/containers/peopleList/peopleList.component.jsx
+++ src/containers/peopleList/peopleList.component.jsx
@@ -1,90 +1,95 @@
-import React, { useEffect, useState } from 'react';
-import Draggable from 'react-draggable';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import PersonSingle from '../../components/personSingle/personSingle.component';
+import React, { useEffect, useState } from "react";
+// import Draggable from 'react-draggable';
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import PersonSingle from "../../components/personSingle/personSingle.component";
 
-import { setPeopleList } from '../../redux/people/people.actions';
-import { selectPeopleList } from '../../redux/people/people.selectors';
+import { setPeopleList } from "../../redux/people/people.actions";
+import { selectPeopleList } from "../../redux/people/people.selectors";
 
-import { peopleData } from '../../sampleData/data';
+import { peopleData } from "../../sampleData/data";
 
 const PeopleList = ({ setPeopleList, selectPeopleList }) => {
+  useEffect(() => {
+    // const x = Boolean(selectPeopleList.length);
+    if (selectPeopleList.length) {
+      //   debugger;
+      return;
+    }
 
-    useEffect(() => {
-        if (selectPeopleList) return
+    const arr = [];
+    peopleData.forEach(({ name, img, id }, index) => {
+      arr.push({
+        name,
+        img,
+        id,
 
-        const arr = []
-        peopleData.forEach(({ name, img, id }, index) => {
-            arr.push({
-                name,
-                img,
-                id,
+        position: index,
+      });
+    });
+    // debugger
 
-                position: index
-            })
-        })
-        // debugger
+    setPeopleList(arr);
 
-        setPeopleList(arr)
+    // return () => {
+    //     cleanup
+    // }
+  }, []);
 
-        // return () => {
-        //     cleanup
-        // }
-    }, [])
+  const [dragId, setDragId] = useState();
 
-    const [dragId, setDragId] = useState();
+  const handleDrag = (ev) => {
+    setDragId(ev.currentTarget.id);
+  };
 
-
-    const handleDrag = (ev) => {
-        setDragId(ev.currentTarget.id);
-    };
-
-    const handleDrop = (ev) => {
-        const dragBox = selectPeopleList.find((box) => Number(box.id) === Number(dragId));
-        const dropBox = selectPeopleList.find((box) => Number(box.id) === Number(ev.currentTarget.id));
-        // debugger
-
-        const dragBoxOrder = dragBox.position;
-        const dropBoxOrder = dropBox.position;
-
-        const newBoxState = selectPeopleList.map((box) => {
-            if (Number(box.id) === Number(dragId)) {
-                box.position = dropBoxOrder;
-            }
-            if (Number(box.id) === Number(ev.currentTarget.id)) {
-                box.position = dragBoxOrder;
-            }
-            console.log(box)
-            return box;
-        });
-        setPeopleList(newBoxState);
-    };
-
-
-    return (
-        <div className="container" style={{ overflowX: 'hidden' }}>
-            <div className="d-flex align-items-center justify-content-between flex-wrap">
-                {
-                    selectPeopleList?.sort((a, b) => a.position - b.position)
-                        .map(person =>
-                            <PersonSingle
-                                key={person.id}
-                                info={person}
-                                handleDrag={handleDrag}
-                                handleDrop={handleDrop} />
-                        )
-                }
-            </div>
-        </div>
+  const handleDrop = (ev) => {
+    const dragBox = selectPeopleList.find(
+      (box) => Number(box.id) === Number(dragId)
     );
+    const dropBox = selectPeopleList.find(
+      (box) => Number(box.id) === Number(ev.currentTarget.id)
+    );
+    // debugger
+
+    const dragBoxOrder = dragBox.position;
+    const dropBoxOrder = dropBox.position;
+
+    const newBoxState = selectPeopleList.map((box) => {
+      if (Number(box.id) === Number(dragId)) {
+        box.position = dropBoxOrder;
+      }
+      if (Number(box.id) === Number(ev.currentTarget.id)) {
+        box.position = dragBoxOrder;
+      }
+      console.log(box);
+      return box;
+    });
+    setPeopleList(newBoxState);
+  };
+
+  return (
+    <div className="container" style={{ overflowX: "hidden" }}>
+      <div className="d-flex align-items-center justify-content-between flex-wrap">
+        {selectPeopleList
+          ?.sort((a, b) => a.position - b.position)
+          .map((person) => (
+            <PersonSingle
+              key={person.id}
+              info={person}
+              handleDrag={handleDrag}
+              handleDrop={handleDrop}
+            />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = createStructuredSelector({
-    selectPeopleList: selectPeopleList
-})
-const mapDispatchToProps = dispatch => ({
-    setPeopleList: (x) => dispatch(setPeopleList(x))
-})
+  selectPeopleList: selectPeopleList,
+});
+const mapDispatchToProps = (dispatch) => ({
+  setPeopleList: (x) => dispatch(setPeopleList(x)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(PeopleList);
